3a258ee3aa4c60fe50e8bfc498703ecdc21cb52d
Danil Kutkevich <danil@kutkevich.org>
2021-12-09 22:47:43 +0300

    docs: copping functions descriptions from fmt package

--- pfmt.go
+++ pfmt.go
@@ -6,21 +6,26 @@ import (
 	"fmt"
 )
 
-// Println it returns the number of bytes written and any write error encountered.
+// Println formats using the default formats for its operands and writes to standard output.
+// Spaces are always added between operands and a newline is appended.
+// It returns the number of bytes written and any write error encountered.
 func Println(a ...interface{}) (n int, errno error) {
 	return fmt.Println(Anys(a...))
 }
 
-// Sprint return string.
+// Sprint formats using the default formats for its operands and returns the resulting string.
+// Spaces are added between operands when neither is a string.
 func Sprint(a ...interface{}) string {
 	return Anys(a...).String()
 }
 
-// Formatter returns string.
+// Formatter returns formatter.
 func Formatter(v interface{}) fmt.Formatter {
 	return Fmt{v: v}
 }
 
+// Format method controls how State and rune are interpreted,
+// and may call Sprint(f) or Fprint(f) etc. to generate its output.
 func (fm Fmt) Format(f fmt.State, c rune) {
 	_, err := f.Write([]byte(Any(fm.v).String()))
 	if err != nil {
@@ -28,6 +33,7 @@ func (fm Fmt) Format(f fmt.State, c rune) {
 	}
 }
 
+// Formatter implements fmt formatter interface.
 type Fmt struct {
 	v interface{}
 }
