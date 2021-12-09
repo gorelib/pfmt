4f18c18a9dfce489ff7851c858d1b4ab37850e4f
Danil Kutkevich <danil@kutkevich.org>
2021-12-09 22:43:11 +0300

    fixing formatter

--- pfmt.go
+++ pfmt.go
@@ -4,35 +4,32 @@ import (
 	"encoding"
 	"encoding/json"
 	"fmt"
-	"time"
 )
-
-// Formatter returns string.
-func Formatter(v interface{}) string {
-	return Sprint(v)
-}
 
 // Println it returns the number of bytes written and any write error encountered.
 func Println(a ...interface{}) (n int, errno error) {
-	return fmt.Println(Sprint(a...))
+	return fmt.Println(Anys(a...))
 }
 
 // Sprint return string.
 func Sprint(a ...interface{}) string {
-	for i := 0; i < len(a); i++ {
-		if a[i] == nil {
-			continue
-		}
+	return Anys(a...).String()
+}
 
-		switch v := a[i].(type) {
-		case *string:
-			a[i] = "*" + *v
+// Formatter returns string.
+func Formatter(v interface{}) fmt.Formatter {
+	return Fmt{v: v}
+}
 
-		case *time.Time:
-			a[i] = "*" + v.String()
-		}
+func (fm Fmt) Format(f fmt.State, c rune) {
+	_, err := f.Write([]byte(Any(fm.v).String()))
+	if err != nil {
+		_, _ = f.Write([]byte(err.Error()))
 	}
-	return fmt.Sprint(a...)
+}
+
+type Fmt struct {
+	v interface{}
 }
 
 // KV is a key-value pair.
